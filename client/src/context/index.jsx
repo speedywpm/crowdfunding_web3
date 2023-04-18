16d14aec7afa92f9a2bce28bfa5bb489c136f067
speedywpm <ernestaszimus@gmail.com>
2023-04-18 14:10:30 +0100

    displaying campaigns

--- client/src/context/index.jsx
+++ client/src/context/index.jsx
@@ -30,8 +30,33 @@ export const StateContextProvider = ({children}) => {
         }  
     }
 
+    const getCampaigns = async () => {
+        const campaigns = await contract.call('getCampaigns');
+
+        const parsedCampaigns = campaigns.map((campaign, i) => ({
+            owner: campaign.owner,
+            title: campaign.title,
+            description: campaign.description,
+            target: ethers.utils.formatEther(campaign.target.toString()),
+            deadline: campaign.deadline.toNumber(),
+            amountCollected: ethers.utils.formatEther(campaign.amountCollected.toString()),
+            image: campaign.image,
+            pId: i
+        }));
+
+        return parsedCampaigns;
+    }
+
+    const getUserCampaigns = async () => {
+        const allCampaigns = await getCampaigns();
+
+        const filteredCampaigns = allCampaigns.filter((campaign) => campaign.owner === address);
+
+        return filteredCampaigns;
+    }
+
     return (
-        <StateContext.Provider value={{address, contract, connect, createCampaign: publishCampaign}}>
+        <StateContext.Provider value={{address, contract, connect, createCampaign: publishCampaign, getCampaigns,getUserCampaigns}}>
            {children} 
         </StateContext.Provider>
     )
--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,8 +1,24 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
+import { useStateContext } from '../context'
+import {DisplayCampaigns} from '../components'
+const Profile = () => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [campaigns, setCampaigns] = useState([]);
 
-function Profile() {
+  const {address, contract, getUserCampaigns} = useStateContext();
+  const fetchCampaigns = async () => {
+    setIsLoading(true);
+    const data = await getUserCampaigns();
+    setCampaigns(data);
+    setIsLoading(false);
+  }
+  useEffect(() => {if(contract) fetchCampaigns();}, [address, contract]);
   return (
-    <div>Profile</div>
+    <DisplayCampaigns
+      title="All campaigns"
+      isLoading={isLoading}
+      campaigns={campaigns}
+    />
   )
 }
 
