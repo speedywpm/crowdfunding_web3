2f4ab053284c844649d1b0af3288b4718844dcf3
speedywpm <ernestaszimus@gmail.com>
2023-04-18 07:21:28 +0100

    add logo to mobile

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -43,7 +43,11 @@ const Navbar = ({isActive, setIsActive, navlinks}) => {
       <div className='sm:hidden flex justify-between items-center relative'>
       <div className='w-[40px] h-[40px] rounded-[10px] bg-[#2c2f32] flex justify-center items-center cursor-pointer'>
             <img src={thirdweb} alt="user" className='w-[60%] h-[60%] object-contain'/>
+            
           </div>
+          <Link to="/">
+            <img src={logo} alt="logo" className='w-[34px] h-[34px] object-contain items-center'/>
+          </Link>
           <img src={menu} alt="menu" className='w-[34px] h-[34px] object-contain cursor-pointer' onClick={() => setToggleDrawer((prev) => !prev)}/>
 
           <div className={`absolute top-[60px] right-0 left-0 bg-[#1c1c24] z-10 shadow-secondary py-4 rounded-[10px] ${!toggleDrawer ? '-translate-y-[100vh]' : 'translate-y-0' } transition-all duration-700`}>
